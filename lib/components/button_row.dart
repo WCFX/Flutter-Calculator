45c55f6df99c204e9e06fa732d3bc04cc39a4815
wcfx <wcfx.dev@gmail.com>
2021-04-17 20:26:37 -0300

    style: 100% da estilizacao concluida

--- lib/components/button_row.dart
+++ lib/components/button_row.dart
@@ -11,9 +11,11 @@ class ButtonRow extends StatelessWidget {
     return Expanded(
       flex: 1,
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: buttons,
-      ),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: buttons.fold(<Widget>[], (list, b) {
+            list.isEmpty ? list.add(b) : list.addAll([SizedBox(width: 1), b]);
+            return list;
+          })),
     );
   }
 }
--- lib/components/keyboard.dart
+++ lib/components/keyboard.dart
@@ -18,24 +18,28 @@ class Keyboard extends StatelessWidget {
             Button.dark(text: '%', callback: callback),
             Button.operation(text: '/', callback: callback),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Button(text: '7', callback: callback),
             Button(text: '8', callback: callback),
             Button(text: '9', callback: callback),
             Button.operation(text: 'X', callback: callback),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Button(text: '4', callback: callback),
             Button(text: '5', callback: callback),
             Button(text: '6', callback: callback),
             Button.operation(text: '-', callback: callback),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Button(text: '1', callback: callback),
             Button(text: '2', callback: callback),
             Button(text: '3', callback: callback),
             Button.operation(text: '+', callback: callback),
           ]),
+          SizedBox(height: 1),
           ButtonRow([
             Button(text: '0', big: true, callback: callback),
             Button(text: ',', callback: callback),
